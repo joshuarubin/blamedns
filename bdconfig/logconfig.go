59e878da3b580ccc84edff8a061f23d731a42816
Joshua Rubin <jrubin@zvelo.com>
2016-06-06 14:01:23 -0600

    update log format

--- bdconfig/logconfig.go
+++ bdconfig/logconfig.go
@@ -2,12 +2,14 @@ package bdconfig
 
 import (
 	"jrubin.io/blamedns/bdconfig/bdtype"
-	"jrubin.io/slog/handlers/auto"
+	"jrubin.io/slog/handlers/json"
+	"jrubin.io/slog/handlers/text"
 )
 
 type LogConfig struct {
-	File  bdtype.LogFile  `toml:"file" cli:",set log filename (stderr, stdout or any file name)"`
+	File  bdtype.LogFile  `toml:"file" cli:",set log filename (stderr, stdout or any file name [if a file, the output will be in json])"`
 	Level bdtype.LogLevel `toml:"level" cli:",set log level (debug, info, warning, error)"`
+	JSON  bool            `toml:"json" cli:",log as json data even to stderr and stdout"`
 }
 
 var defaultLogConfig = LogConfig{
@@ -17,7 +19,11 @@ var defaultLogConfig = LogConfig{
 
 func (cfg LogConfig) Init(root *Config) {
 	logger := root.Logger
-	logger.RegisterHandler(cfg.Level.Level(), auto.New(cfg.File.Writer))
+	if cfg.File.File() != nil || cfg.JSON {
+		logger.RegisterHandler(cfg.Level.Level(), json.New(cfg.File.Writer))
+	} else {
+		logger.RegisterHandler(cfg.Level.Level(), text.New(cfg.File.Writer))
+	}
 	logger.WithField("level", cfg.Level).Debug("log level set")
 }
 
