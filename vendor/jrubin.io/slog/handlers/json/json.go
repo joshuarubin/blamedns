b4a92ae93ac6c213125222b534c307417bcb8516
Joshua Rubin <jrubin@zvelo.com>
2016-06-14 15:41:04 -0600

    update deps

--- vendor/jrubin.io/slog/handlers/json/json.go
+++ vendor/jrubin.io/slog/handlers/json/json.go
@@ -3,15 +3,23 @@ package json
 
 import (
 	j "encoding/json"
+	"fmt"
 	"io"
 	"os"
 	"sync"
+	"time"
 
 	"jrubin.io/slog"
 )
 
 // Default handler outputting to stderr.
 var Default = New(os.Stderr)
+
+// Logger returns a logger configured to output json at level or higher to
+// stderr.
+func Logger(level slog.Level) *slog.Logger {
+	return slog.New().RegisterHandler(level, Default)
+}
 
 // Handler implementation.
 type Handler struct {
@@ -26,9 +34,38 @@ func New(w io.Writer) *Handler {
 	}
 }
 
+type entry struct {
+	Fields  slog.Fields `json:"fields"`
+	Level   slog.Level  `json:"level"`
+	Time    time.Time   `json:"time"`
+	Message string      `json:"msg"`
+}
+
+func newEntry(e *slog.Entry) *entry {
+	ret := &entry{
+		Fields:  slog.Fields{},
+		Level:   e.Level,
+		Time:    e.Time,
+		Message: e.Message,
+	}
+
+	for key, value := range e.Fields {
+		switch value := value.(type) {
+		case fmt.Stringer:
+			ret.Fields[key] = value.String()
+		case error:
+			ret.Fields[key] = value.Error()
+		default:
+			ret.Fields[key] = value
+		}
+	}
+
+	return ret
+}
+
 // HandleLog implements slog.Handler.
 func (h *Handler) HandleLog(e *slog.Entry) error {
 	h.mu.Lock()
 	defer h.mu.Unlock()
-	return h.enc.Encode(e)
+	return h.enc.Encode(newEntry(e))
 }
